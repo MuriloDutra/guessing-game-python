76a31be76428f99397605f98fdcc2ddedd99113f
Murilo <MuriloDutraProjects@gmail.com>
2021-08-25 19:48:35 -0300

    Moduling the project

---
+++ guessingGame.py
@@ -0,0 +1,47 @@
+import random
+
+
+def play():
+    print("****************************")
+    print("WELCOME TO THE GUESSING GAME")
+    print("****************************")
+
+    secret_number = random.randrange(1, 101)
+    maximum_of_attempts = 0
+    score = 1000
+
+    print("(1) Easy (2) Medium (3) Hard")
+    difficulty_level = int(input("Choose a difficulty level: "))
+
+    if (difficulty_level == 1):
+        maximum_of_attempts = 20
+    elif (difficulty_level == 2):
+        maximum_of_attempts = 10
+    else:
+        maximum_of_attempts = 5
+
+    for shout in range(1, maximum_of_attempts + 1):
+        print("\n")
+        print(
+            f"# Attempt {shout} of {maximum_of_attempts}")  # equivalent print("Tentativa {} de {}".format(shout, maximum_of_attempts))
+
+        user_attempt = int(input("Type a number between 1 and 100: "))
+
+        if (user_attempt < 1 or user_attempt > 100):
+            print("Invalid number, you must type a number between 1 and 100.")
+            continue
+
+        user_won = (user_attempt == secret_number)
+        user_attempt_is_bigger = (user_attempt > secret_number)
+        user_attempt_is_smaller = (user_attempt < secret_number)
+
+        if (user_won):
+            print(f"You WON! Your score: {score}.")
+            break
+        else:
+            if (user_attempt_is_bigger):
+                print("You missed! Your attempt was HIGHER than the secret number.")
+            elif (user_attempt_is_smaller):
+                print("You missed! Your attempt was LOWER than the secret number.")
+            lost_points = abs(secret_number - user_attempt)
+            score = score - lost_points
--- index.py
+++ index.py
@@ -1,44 +1,15 @@
-import random
+import guessingGame
+import formattingNumbers
 
-print("********************************")
-print("BEM-VINDO AO JOGO DE ADIVINHAÇÃO")
-print("********************************")
+print("*****")
+print("FILES")
+print("*****")
 
-secret_number = random.randrange(1, 101)
-maximum_of_attempts = 0
-score = 1000
+print("(1) guessingGame.py (2) formattingNumbers.py")
 
-print("(1) Fácil (2) Médio (3) Difícil")
-difficulty_level = int(input("Qual o nível de dificuldade?"))
+chosen_file = int(input("Choose a file: "))
 
-if(difficulty_level == 1):
-    maximum_of_attempts = 20
-elif(difficulty_level == 2):
-    maximum_of_attempts = 10
-else:
-    maximum_of_attempts = 5
-
-for round in range(1, maximum_of_attempts + 1):
-    print("\n")
-    print(f"# Tentativa {round} de {maximum_of_attempts}") #equivalent print("Tentativa {} de {}".format(round, maximum_of_attempts))
-
-    user_attempt = int(input("Digite um número entre 1 e 100: "))
-
-    if(user_attempt < 1 or user_attempt > 100):
-        print("Número inválido, digite um número entre 1 e 100.")
-        continue
-
-    user_won = (user_attempt == secret_number)
-    user_attempt_is_bigger = (user_attempt > secret_number)
-    user_attempt_is_smaller = (user_attempt < secret_number)
-
-    if(user_won):
-        print(f"Você acertou! Seu resultado: {score} pontos.")
-        break
-    else:
-        if(user_attempt_is_bigger):
-            print("Você errou. Seu chute foi MAIOR que o número secreto")
-        elif(user_attempt_is_smaller):
-            print("Você errou. Seu chute foi MENOR que o número secreto")
-        lost_points = abs(secret_number - user_attempt)
-        score = score - lost_points
+if(chosen_file == 1):
+    guessingGame.play()
+elif(chosen_file == 2):
+    formattingNumbers.format()
